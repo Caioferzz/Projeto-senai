c4d56ff31c6f4334b9fc26de3f266657a279e03d
CAIO FERNANDES BARBOSA <25203662@CFP127ALUNO.LOCAL>
2025-08-25 16:29:30 -0300

    Finalizada a aula sobre GIT

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 	public static void main(string[] args){
 		System.out.println("olá, galera!!");
-		System.out.ptintln("Estamos no SENAI!")
-		System.out.ptintln("E o GIT funciona!!")
+		System.out.ptintln("Estamos no SENAI!");
+		System.out.ptintln("E o GIT funciona!!");
 	
 	
 	}
